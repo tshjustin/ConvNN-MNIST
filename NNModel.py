f0f04bc2535d88438c8e835ec00fdd963b998b00
Justin <j.tann2305@gmail.com>
2024-01-21 14:25:27 +0800

    updated model & GUI

---
+++ NNModel.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+class DeepNN(nn.Module):
+    def __init__(self):
+        super(DeepNN, self).__init__()
+        self.fc1 = nn.Linear(28*28, 128)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 32)
+        self.fc4 = nn.Linear(32, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
